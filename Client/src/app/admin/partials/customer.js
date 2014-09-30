4744a131cdc1887f8f9284a3454ce40422225dcf
Dryvenn <dryvenn@gmail.com>
2014-09-30 16:03:21 +0200

    finished to allow a user to add a new customer TODO: server

--- Client/src/app/admin/partials/customer.js
+++ Client/src/app/admin/partials/customer.js
@@ -1,31 +1,54 @@
 angular.module('babar.admin.customer', [
     'babar.server'
 ])
-    .controller('AdmCustomerCtrl', ['$scope', '$stateParams', 'Server', function($scope, $stateParams, Server){
+    .controller('AdmCustomerCtrl', ['$scope', '$state', '$stateParams', 'Server', function($scope, $state, $stateParams, Server){
 
+	console.log($scope);
+        this.isReadOnly = true;
+        this.toWritingMode = function(){
+            this.isReadOnly = false;
+            
+        };
 	
 	this.current = null;
 	this.statuses = ['Customer', 'VIP', 'Barman', 'Barchief'];
 	this.status = this.statuses[0];
 	
-        var response = Server.getAdminDetails('customer', $stateParams.id);
-        if(response.status !== 200){
-            //TODO ('cause a bit extreme)
-            $state.go('sell');
-        }else{
-            response.data.then(function(result){
-		//update the current user
-		$scope.admcst.current = result;
-		//initiate to the right status (that complicated 'cause ngOptions understand references instead of values)
-		//$scope.admcst.status = $scope.admcst.statuses[$scope.admcst.statuses.indexOf(result.status)];
-		
-            });
-        }
-	
-        this.isReadOnly = true;
-	this.toWritingMode = function(){
+	//init of the current customer
+	if($stateParams.id === -1){ //we're in new user mode
 	    this.isReadOnly = false;
-	    
+	}else{ //user exists
+            var response = Server.getAdminDetails('customer', $stateParams.id);
+            if(response.status !== 200){
+		//TODO ('cause a bit extreme)
+		$state.go('sell');
+            }else{
+		response.data.then(function(result){
+		    //update the current user
+		    $scope.admcst.current = result;
+		    //initiate to the right status (that complicated 'cause ngOptions understand references instead of values) TODO: uncomment when ready
+		    //$scope.admcst.status = $scope.admcst.statuses[$scope.admcst.statuses.indexOf(result.status)];
+		    
+		});
+            }
+	}
+
+	this.add = function(){
+	    $scope.$parent.admin.currentItem = null;
+	    $state.go('admin.customer', {id:-1});
+	};
+	
+	this.confirm = function(){
+	    //TODO make a post
+	    //Confirm only if the form is untouched or touched but valid
+	    if($scope.form.$pristine || $scope.form.$valid){
+		this.isReadOnly = true;
+	    }
+        };
+
+	this.cancel = function(){
+	    //TODO make a get
+	    this.isReadOnly = true;
 	};
 	
     }]);
