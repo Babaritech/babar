525266740dc1e6a7d29454f01853ae4837cd1b45
Iansus <jean.iansus@gmail.com>
2014-10-03 15:38:51 +0200

    minor add

--- Server/newbabar.sql
+++ Server/newbabar.sql
@@ -2,22 +2,33 @@
 -- version 4.2.9
 -- http://www.phpmyadmin.net
 --
--- Client :  localhost
--- Généré le :  Ven 03 Octobre 2014 à 03:28
--- Version du serveur :  5.5.38-0+wheezy1
--- Version de PHP :  5.4.4-14+deb7u14
+-- Host: localhost
+-- Generation Time: Oct 03, 2014 at 03:38 PM
+-- Server version: 5.5.38-0+wheezy1
+-- PHP Version: 5.4.4-14+deb7u14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Base de données :  `newbabar`
+-- Database: `newbabar`
 --
 
 -- --------------------------------------------------------
 
 --
--- Doublure de structure pour la vue `balances`
+-- Table structure for table `actions`
+--
+
+CREATE TABLE IF NOT EXISTS `actions` (
+`id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `balances`
 --
 CREATE TABLE IF NOT EXISTS `balances` (
 `customer_id` int(11)
@@ -26,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `balances` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `customers`
+-- Table structure for table `customers`
 --
 
 CREATE TABLE IF NOT EXISTS `customers` (
@@ -37,12 +48,12 @@ CREATE TABLE IF NOT EXISTS `customers` (
   `nickname` varchar(255) NOT NULL,
   `password` varchar(40) NOT NULL,
   `email` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COMMENT='Table des clients';
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COMMENT='Table des clients';
 
 -- --------------------------------------------------------
 
 --
--- Doublure de structure pour la vue `customer_entries`
+-- Stand-in structure for view `customer_entries`
 --
 CREATE TABLE IF NOT EXISTS `customer_entries` (
 `customer_id` int(11)
@@ -51,7 +62,7 @@ CREATE TABLE IF NOT EXISTS `customer_entries` (
 -- --------------------------------------------------------
 
 --
--- Doublure de structure pour la vue `customer_sells`
+-- Stand-in structure for view `customer_sells`
 --
 CREATE TABLE IF NOT EXISTS `customer_sells` (
 `customer_id` int(11)
@@ -60,7 +71,7 @@ CREATE TABLE IF NOT EXISTS `customer_sells` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `drinks`
+-- Table structure for table `drinks`
 --
 
 CREATE TABLE IF NOT EXISTS `drinks` (
@@ -73,7 +84,17 @@ CREATE TABLE IF NOT EXISTS `drinks` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `entries`
+-- Stand-in structure for view `effective_rights`
+--
+CREATE TABLE IF NOT EXISTS `effective_rights` (
+`action_name` varchar(255)
+,`status_name` varchar(20)
+,`right` enum('allow','deny')
+);
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `entries`
 --
 
 CREATE TABLE IF NOT EXISTS `entries` (
@@ -87,14 +108,27 @@ CREATE TABLE IF NOT EXISTS `entries` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `sells`
+-- Table structure for table `rights`
+--
+
+CREATE TABLE IF NOT EXISTS `rights` (
+`id` int(11) NOT NULL,
+  `action_id` int(11) NOT NULL,
+  `status_id` int(11) NOT NULL,
+  `right` enum('allow','deny') NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sells`
 --
 
 CREATE TABLE IF NOT EXISTS `sells` (
 `id` int(11) NOT NULL COMMENT 'ID de la vente',
   `customer_id` int(11) NOT NULL COMMENT 'ID du client',
   `drink_id` int(11) DEFAULT NULL COMMENT 'ID du produit',
-  `quantity` int(11) NOT NULL COMMENT 'Quantité achetée',
+  `quantity` int(11) NOT NULL COMMENT 'QuantitÃ© achetÃ©e',
   `price` float(5,2) NOT NULL COMMENT 'Prix',
   `date` varchar(30) NOT NULL COMMENT 'Date de la vente'
 ) ENGINE=InnoDB AUTO_INCREMENT=53828 DEFAULT CHARSET=utf8 COMMENT='Table des ventes';
@@ -102,19 +136,19 @@ CREATE TABLE IF NOT EXISTS `sells` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `status`
+-- Table structure for table `status`
 --
 
 CREATE TABLE IF NOT EXISTS `status` (
 `id` int(11) NOT NULL,
   `name` varchar(20) NOT NULL COMMENT 'ID du status',
-  `overdraft` float NOT NULL COMMENT 'Découvert autorisé'
+  `overdraft` float NOT NULL COMMENT 'DÃ©couvert autorisÃ©'
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='Table des status';
 
 -- --------------------------------------------------------
 
 --
--- Doublure de structure pour la vue `total_entries`
+-- Stand-in structure for view `total_entries`
 --
 CREATE TABLE IF NOT EXISTS `total_entries` (
 `customer_id` int(11)
@@ -123,7 +157,7 @@ CREATE TABLE IF NOT EXISTS `total_entries` (
 -- --------------------------------------------------------
 
 --
--- Doublure de structure pour la vue `total_sells`
+-- Stand-in structure for view `total_sells`
 --
 CREATE TABLE IF NOT EXISTS `total_sells` (
 `customer_id` int(11)
@@ -132,7 +166,7 @@ CREATE TABLE IF NOT EXISTS `total_sells` (
 -- --------------------------------------------------------
 
 --
--- Structure de la vue `balances`
+-- Structure for view `balances`
 --
 DROP TABLE IF EXISTS `balances`;
 
@@ -141,7 +175,7 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 -- --------------------------------------------------------
 
 --
--- Structure de la vue `customer_entries`
+-- Structure for view `customer_entries`
 --
 DROP TABLE IF EXISTS `customer_entries`;
 
@@ -150,7 +184,7 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 -- --------------------------------------------------------
 
 --
--- Structure de la vue `customer_sells`
+-- Structure for view `customer_sells`
 --
 DROP TABLE IF EXISTS `customer_sells`;
 
@@ -159,7 +193,16 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 -- --------------------------------------------------------
 
 --
--- Structure de la vue `total_entries`
+-- Structure for view `effective_rights`
+--
+DROP TABLE IF EXISTS `effective_rights`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `effective_rights` AS select `a`.`name` AS `action_name`,`s`.`name` AS `status_name`,`r`.`right` AS `right` from ((`actions` `a` join `rights` `r`) join `status` `s`) where ((`a`.`id` = `r`.`action_id`) and (`s`.`id` = `r`.`status_id`)) order by `a`.`name`,`s`.`name`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `total_entries`
 --
 DROP TABLE IF EXISTS `total_entries`;
 
@@ -168,94 +211,123 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 -- --------------------------------------------------------
 
 --
--- Structure de la vue `total_sells`
+-- Structure for view `total_sells`
 --
 DROP TABLE IF EXISTS `total_sells`;
 
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `total_sells` AS select `customer_sells`.`customer_id` AS `customer_id`,sum(ifnull(`customer_sells`.`amount`,0)) AS `total` from `customer_sells` group by `customer_sells`.`customer_id`;
 
 --
--- Index pour les tables exportées
+-- Indexes for dumped tables
 --
 
 --
--- Index pour la table `customers`
+-- Indexes for table `actions`
+--
+ALTER TABLE `actions`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `customers`
 --
 ALTER TABLE `customers`
  ADD PRIMARY KEY (`id`), ADD KEY `status_id` (`status_id`);
 
 --
--- Index pour la table `drinks`
+-- Indexes for table `drinks`
 --
 ALTER TABLE `drinks`
  ADD PRIMARY KEY (`id`);
 
 --
--- Index pour la table `entries`
+-- Indexes for table `entries`
 --
 ALTER TABLE `entries`
  ADD PRIMARY KEY (`id`), ADD KEY `customer` (`customer_id`), ADD KEY `debitant` (`debitant_id`);
 
 --
--- Index pour la table `sells`
+-- Indexes for table `rights`
+--
+ALTER TABLE `rights`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `action` (`action_id`), ADD UNIQUE KEY `status` (`status_id`);
+
+--
+-- Indexes for table `sells`
 --
 ALTER TABLE `sells`
  ADD PRIMARY KEY (`id`), ADD KEY `drink` (`drink_id`), ADD KEY `customer` (`customer_id`);
 
 --
--- Index pour la table `status`
+-- Indexes for table `status`
 --
 ALTER TABLE `status`
  ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT pour les tables exportées
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT pour la table `customers`
+-- AUTO_INCREMENT for table `actions`
+--
+ALTER TABLE `actions`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=48;
+--
+-- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID du client',AUTO_INCREMENT=6;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID du client',AUTO_INCREMENT=7;
 --
--- AUTO_INCREMENT pour la table `drinks`
+-- AUTO_INCREMENT for table `drinks`
 --
 ALTER TABLE `drinks`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID du produit',AUTO_INCREMENT=130;
 --
--- AUTO_INCREMENT pour la table `entries`
+-- AUTO_INCREMENT for table `entries`
 --
 ALTER TABLE `entries`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID du paiement',AUTO_INCREMENT=12502;
 --
--- AUTO_INCREMENT pour la table `sells`
+-- AUTO_INCREMENT for table `rights`
+--
+ALTER TABLE `rights`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `sells`
 --
 ALTER TABLE `sells`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID de la vente',AUTO_INCREMENT=53828;
 --
--- AUTO_INCREMENT pour la table `status`
+-- AUTO_INCREMENT for table `status`
 --
 ALTER TABLE `status`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
--- Contraintes pour les tables exportées
+-- Constraints for dumped tables
 --
 
 --
--- Contraintes pour la table `customers`
+-- Constraints for table `customers`
 --
 ALTER TABLE `customers`
 ADD CONSTRAINT `customer_to_status` FOREIGN KEY (`status_id`) REFERENCES `status` (`id`) ON UPDATE CASCADE;
 
 --
--- Contraintes pour la table `entries`
+-- Constraints for table `entries`
 --
 ALTER TABLE `entries`
 ADD CONSTRAINT `entry_to_customer` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
 ADD CONSTRAINT `entry_to_debitant` FOREIGN KEY (`debitant_id`) REFERENCES `customers` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
--- Contraintes pour la table `sells`
+-- Constraints for table `rights`
+--
+ALTER TABLE `rights`
+ADD CONSTRAINT `rights_to_status` FOREIGN KEY (`status_id`) REFERENCES `status` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `rights_to_action` FOREIGN KEY (`action_id`) REFERENCES `actions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `sells`
 --
 ALTER TABLE `sells`
 ADD CONSTRAINT `sell_to_customer` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
