793e25f3397085866b5d89773b426ba6a5df2554
Dryvenn <dryvenn@gmail.com>
2015-02-16 10:49:45 +0100

    deleted dead code

--- Client/src/app/app.spec.js
+++ Client/src/app/app.spec.js
@@ -1,26 +1,5 @@
-// ngboilerplate's config
-/*describe( 'AppCtrl', function() {
-  describe( 'isCurrentUrl', function() {
-    var AppCtrl, $location, $scope;
-
-    beforeEach( module( 'babar' ) );
-
-    beforeEach( inject( function( $controller, _$location_, $rootScope ) {
-      $location = _$location_;
-	$scope = $rootScope.$new();
-      AppCtrl = $controller( 'AppCtrl', { $location: $location, $scope: $scope });
-    }));
-
-    it( 'should pass a dummy test', inject( function() {
-      expect( AppCtrl ).toBeTruthy();
-    }));
-  });
-});
-*/
-
 describe( 'AppCtrl', function() {
     describe( 'isCurrentUrl', function() {
-
 	it( 'should pass a dummy test', inject( function() {
 	    expect( true ).toBeTruthy();
 	}));
