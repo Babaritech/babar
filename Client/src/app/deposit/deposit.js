25c106745bcab0229255ecc38648c0df58518a20
Dryvenn <dryvenn@gmail.com>
2015-02-22 18:00:00 +0100

    deposit designed

--- Client/src/app/deposit/deposit.js
+++ Client/src/app/deposit/deposit.js
@@ -1,63 +1,35 @@
-angular.module('babar.deposit', [
+angular
+    .module('babar.deposit', [
     'babar.server',
     'cfp.hotkeys'
 ])
-    .controller('DepositCtrl', ['$rootScope', '$scope', 'Server', 'hotkeys', function($rootScope, $scope, Server, Hotkeys){
+    .controller('DepositCtrl', function($rootScope, $scope, Server, customer, user){
 	
-	
-        this.customer = $scope.ngDialogData[0];
 
+	this.customer = customer.details;
+	this.user = user;
 	this.money = 0;
 
 	this.error = "";
+        this.cancel = function() {
+            new Toast().display('deposit cancelled');
+            $mdDialog.cancel();
+        };
 	
-	$scope.make = function(){
-	    if($scope.deposit.money === 0){
-		$scope.deposit.error = "Can't deposit no money.";
-	    }else if($scope.deposit.money > 100){
-		$scope.deposit.error = "Can't deposit more than 100€ at a time.";
+	this.confirm = function(){
+	    if(this.money === 0){
+		this.error = "Can't deposit no money.";
+	    }else if(this.money > 100){
+		this.error = "Can't deposit more than 100€ at a time.";
 	    }else{
-		$scope.deposit.disableHotkeys();
 		Server.create.deposit({
-		    customer: $scope.deposit.customer,
-		    amount: $scope.deposit.money
+		    customer: this.customer,
+		    amount: this.money,
+		    user: this.user
 		}).then(function() {
 		    $rootScope.$emit('refresh', {'from': 'deposit', 'to': 'all'});
-                    $scope.closeThisDialog('deposited');
+		    $mdDialog.hide();
                 });
 	    }
         };
-	$scope.cancel = function(){
-	    $scope.deposit.disableHotkeys();
-            $scope.closeThisDialog('cancelled');
-        };
-
-	$scope.selectField = function(){
-	    document.getElementById('depositInput').focus();
-        };
-
-        //Let's set up some hotkeys !
-	Hotkeys.add({
-            combo: 'enter',
-            description: 'Make the deposit',
-            callback: $scope.make,
-	    allowIn: ['INPUT']
-        });
-        Hotkeys.add({
-            combo: 'escape',
-            description: 'Cancel the deposit',
-            callback: $scope.cancel,
-	    allowIn: ['INPUT']
-        });
-        Hotkeys.add({
-	    combo: 'tab',
-	    description: 'Select the right field',
-	    callback: $scope.selectField,
-	    allowIn: ['INPUT']
-	});
-	this.disableHotkeys = function(){
-            Hotkeys.del('tab');
-            Hotkeys.del('enter');
-            Hotkeys.del('escape');
-        };
-    }]);
+    });
