51494420985067c93a4a0731a10d2dbf50d974ef
Dryvenn <dryvenn@gmail.com>
2015-02-24 21:16:39 +0100

    speed improvement by cutting the list to 5 elements

--- Client/src/app/utils.js
+++ Client/src/app/utils.js
@@ -4,24 +4,28 @@ angular
     .filter('search', function() {
         return function(input, keyword){
             return input.filter(function(ival, ind, arr) {
+		// turn off this option if this is too slow
+		var TEST_CHAR_BY_CHAR = true;
 		// if there're special elements, keep them
 		if(ival.id < 0) {
 		    return true;
 		}
 		else {
+		    if(TEST_CHAR_BY_CHAR) {
+			//test if all the letters of the keyword are included in the name
+                        return keyword.split('').every(function(kval, ind, arr) {
+                             return ival.name.toLowerCase().indexOf(kval.toLowerCase()) > -1;
+                        });
+		    }
+		    else {
                     // test if keyword is included in the name
                     return ival.name.toLowerCase().indexOf(keyword.toLowerCase()) > -1;
-
-                    //test if all the letters of the keyword are included in the name
-                    //choose the above if this is too slow
-                    // return keyword.split('').every(function(kval, ind, arr) {
-		    // 	return ival.name.toLowerCase().indexOf(kval.toLowerCase()) > -1;
-                    // });
+		    }
+                    
 		}
             });
         };
     })
-
     .filter('select', function() {
         //update each item 'active' value for item highlighting
         return function(input, index) {
@@ -33,5 +37,10 @@ angular
                 }
                 return val;
             });
+        };
+    })
+    .filter('cut', function() {
+	return function(input, count) {
+	    return input.splice(0, parseInt(count, 10) || 5);
         };
     });
