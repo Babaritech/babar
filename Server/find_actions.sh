bac40d77edc5da1cd5a43d8fa345a64598dc725e
Perceval <perceval.arenou@gmail.com>
2015-07-02 20:43:10 +0200

    find actions

--- Server/find_actions.sh
+++ Server/find_actions.sh
@@ -4,7 +4,7 @@ for file in *.php; do
 	filename=$(echo $file | sed 's/.php//')
 
 	cat "$file" | grep "case '" | while read action; do
-		actionName=$(echo $action | sed -r "s/case '(.+)':/\1/g")
+		actionName=$(echo $action | sed -E "s/case '(.+)':/\1/g")
 		echo "$filename-$actionName"
 	done
 done	
